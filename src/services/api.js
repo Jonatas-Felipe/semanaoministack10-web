f4484e3340ee19426c4f03f3a93485ea549d7c14
Jonatas <jonatas-nandes@hotmail.com>
2020-01-25 22:58:49 -0300

    variavel ambiente do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://semanaoministack10-backend.herokuapp.com"
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
